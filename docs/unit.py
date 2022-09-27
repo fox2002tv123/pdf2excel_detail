5a6b31a22f608e2caa403823249c7fb31c6f72a5
fox2002tv123 <56084397+fox2002tv123@users.noreply.github.com>
2022-09-28 07:04:14 +0800

    Update unit.py
    添加 配件全赔付情况

--- docs/unit.py
+++ docs/unit.py
@@ -1,6 +1,5 @@
 
 
-
 #
 # open读取txt文件
 
@@ -62,84 +61,104 @@ def run(data):
     #
     # 0.re提取每个特征-生成段落
     import pandas as pd
-    
-    
+
     import re
-    detail_list=re.findall(r'[^_]+',data)[:-1]
+    detail_list = re.findall(r'[^_]+', data)[:-1]
     detail_list.__len__()
 
-    res_list=[]
+    res_list = []
     for i in detail_list:
-        
-        
-        
-        # 
+
+        concat_list = []
+
+        #
         # 1提取工时行
-        FRU=re.findall(r'(\S{10}) (\d{7}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)',i)
-        FRU
-        df_FRU=pd.DataFrame(FRU,columns=list('ABCDFHKL'))
-        df_FRU
-
-        # 
-        #   2.提取零件行-正常配件
-        part_normal=re.findall(r'(\S{10}) (\S{11}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)',i)
-        part_normal
-        df_part_normal=pd.DataFrame(part_normal,columns=list('ABCDEFGJKL'))
-        df_part_normal
-
-        # 
-        # 3.提取零件行-BSI配件
-        part_BSI=re.findall(r'(8\S{8}6) (\S{11}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)',i)
-        part_BSI
-        df_part_BSI=pd.DataFrame(part_BSI,columns=list('ABCDFGKL'))
-        df_part_BSI
-
-        # 
-        # 4.提取辅料
-        sub_4=re.findall(r'(\S{10}) (\S{1}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)',i)
-        sub_4
-        df_sub_4=pd.DataFrame(sub_4,columns=list('ABCDFIKL'))
-        df_sub_4
+        FRU = re.findall(
+            r'(\S{10}) (\d{7}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)', i)
+        if FRU:
+            df_FRU = pd.DataFrame(FRU, columns=list('ABCDFHKL'))
+            concat_list.append(df_FRU)
         
-        # 5.提取F111
-        F111=re.findall(r'(9999001386) (4) (\S+) (\S+) (\S+) (\S+) (\S+)',i)
+
+        #
+        #   2.提取零件行-正常配件
+        part_normal = re.findall(
+            r'(\S{10}) (\S{11}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)', i)
+        part_normal
+        if part_normal:
+            df_part_normal = pd.DataFrame(
+                part_normal, columns=list('ABCDEFGJKL'))
+            concat_list.append(df_part_normal)
+        #
+
+        #
+        # 3.提取零件行-BSI配件
+        part_BSI = re.findall(
+            r'(8\S{8}6) (\S{11}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)', i)
+        part_BSI
+        df_part_BSI = pd.DataFrame(part_BSI, columns=list('ABCDFGKL'))
+        df_part_BSI
+        # 3.提取零件行-BSI配件
+        part_BSI = re.findall(
+            r'(8\S{8}6) (\S{11}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)', i)
+        part_BSI
+        if part_BSI:
+            df_part_BSI = pd.DataFrame(part_BSI, columns=list('ABCDFGKL'))
+            concat_list.append(df_part_BSI)
+
+        # 4.提取零件行-全陪付配件
+        #  re 设置为多行模式
+        # re.MULTILINE
+        df_part_short_detail = re.findall(
+            r'^(\S{10}) (\S{11}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)$', i, re.MULTILINE)
+        df_part_short_detail
+        if df_part_short_detail:
+            df_short = pd.DataFrame(
+                df_part_short_detail, columns=list('ABCDFGJKL'))
+            concat_list.append(df_short)
+        #
+        # 5.提取辅料
+        sub_4 = re.findall(
+            r'(\S{10}) (\S{1}) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+)', i)
+        sub_4
+        if sub_4:
+            df_sub_4 = pd.DataFrame(sub_4, columns=list('ABCDFIKL'))
+            concat_list.append(df_sub_4)
+
+
+        # 6.提取F111
+        F111 = re.findall(r'(9999001386) (4) (\S+) (\S+) (\S+) (\S+) (\S+)', i)
         F111
-        df_F111=pd.DataFrame(F111,columns=list('ABCFIKL'))
-        df_F111
+        if F111:
+            df_F111 = pd.DataFrame(F111, columns=list('ABCFIKL'))
+            concat_list.append(df_F111)
 
-        # 
-        # 6.找到单行的表头
-        p=r'(\d{5,6}) (\S{17}) (\S{6})'
-        one_head=re.findall(p,i)
+        #
+        # 7.找到单行的表头
+        p = r'(\d{5,6}) (\S{17}) (\S{6})'
+        one_head = re.findall(p, i)
         one_head
-        claim,vin,dwp=one_head[0]
-        claim,vin,dwp
+        claim, vin, dwp = one_head[0]
+        claim, vin, dwp
 
-        # 
+        #
         # 建立一个空的dataframe，只包括表头-作用是避免漏掉没有数据的列
-        df_na=pd.DataFrame(columns=list('ABCDEFGHIJKL'))
-        df_na
+        df_na = pd.DataFrame(columns=list('ABCDEFGHIJKL'))
+        concat_list.append(df_na)
 
-        # 
+        #
         # 拼接dataframe
-        res_one_part=pd.concat([df_FRU,df_part_normal,df_part_BSI,df_sub_4,df_F111,df_na],axis=0)
+        res_one_part = pd.concat(concat_list, axis=0)
         # column 按照字母排序
-        res_one_part.sort_index(axis=1,inplace=True)
+        res_one_part.sort_index(axis=1, inplace=True)
         # claim vin dwp 插入前三列
-        res_one_part.insert(0,'claim',claim)
-        res_one_part.insert(1,'vin',vin)
-        res_one_part.insert(2,'dwp',dwp)
+        res_one_part.insert(0, 'claim', claim)
+        res_one_part.insert(1, 'vin', vin)
+        res_one_part.insert(2, 'dwp', dwp)
         res_list.append(res_one_part)
 
-    res1=pd.concat(res_list,axis=0)
-    # 将A-L列转换为数值
-    # res[list('ABCDEFJHIJKL')]=res[list('ABCDEFJHIJKL')].apply(pd.to_numeric,errors='ignore')
-    res=res1.to_csv(index=False)
-    # df1=res.copy()
-    # new_columns=['DWP','CLAIM','VIN','NO','DATE','1','2','3','4','5','TOTAL']
-    # df1.rename(columns=dict(zip(alist,new_columns)),inplace=True)
-    # df1[new_columns].head()
-    # collect_list=['DWP','CLAIM','VIN','NO','DATE','detail_part','detail_FRU','detail_sublit','detail_handcost','detail_tax','TOTAL']
-    # res=df1[collect_list].to_csv(index=False)
-    return res_number,res_date,s,res # 返回对账单序列号和对账单日期
-    # return df.head()
+    res1 = pd.concat(res_list, axis=0)
+    res = res1.to_csv(index=False)
+    
+    return res_number, res_date, s, res  # 返回对账单序列号和对账单日期
+    
